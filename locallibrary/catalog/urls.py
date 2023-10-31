a26e1acd422e240e4f839048daacf30c6c0fe1e0
WorthyFire <worthyfire@vk.com>
2023-10-31 10:28:45 +0700

    Добавлеине My Borowed

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 
@@ -7,5 +8,11 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('author/<int:pk>',views.AuthorDetailView.as_view(), name='author-detail'),
+    path('author/<int:pk>',
+         views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 from django.shortcuts import render
 from django.views import generic
@@ -58,4 +59,12 @@ class AuthorDetailView(generic.DetailView):
 
     model = Author
 
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
