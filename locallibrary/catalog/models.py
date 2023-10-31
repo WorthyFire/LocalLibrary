7767209345fa7da2afb3925a47fc8d6ccb324daa
WorthyFire <worthyfire@vk.com>
2023-10-31 10:40:41 +0700

    Выполненное "Испытай себя" часть 8. Добавление у администратора просмотра взятых книг определенным пользователем. Проверка на функциональность пройдена

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -69,6 +69,8 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
+
+    permissions = (("can_mark_returned", "Set book as returned"),)
     def __str__(self):
         return f'{self.book.title}, {self.get_status_display()}, Due:{self.due_back}, ID: {self.id}'
 
